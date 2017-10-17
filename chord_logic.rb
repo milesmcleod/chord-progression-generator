b9659a858440160adc5265badae3111d3a4271b0
Your Name <you@example.com>
2017-10-16 23:23:09 -0700

    Begin chord logic

---
+++ chord_logic.rb
@@ -0,0 +1,12 @@
+#thinking for major
+
+#starting with the 1 is often a good idea and then only using it
+#maybe 0 or 1 more time in the course of the generated progression
+#
+
+
+#for jazz, we need to start with 2-5-1 and maybe 6-2-5-1, but also
+#need to pepper in secondary dominants, and dominant 7 chords
+
+#it can't just be some modal wonk shit
+#although it probably will be
